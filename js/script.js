63b35a4593221d2766ca78cb9d41d3f792aaf0af
dev.javad.javadzadeh <javad@MacBook-Pro-Namig.local>
2021-04-28 01:02:16 +0300

    change background. made footer space

--- js/script.js
+++ js/script.js
@@ -1,74 +1,27 @@
-
-//При ширине экрана <500px показывает расписание соответствующей школы
-
-//Показать расписание "Школа разработки интерфейсов"
-showColDevelpInterface = function (){
-		$('.dev').css("display","table");
-		$('.mob-dev').css("display","none");
-		$('.mob-des').css("display","none");
-		$('.empty').css("display","none");
-}
-//Показать расписание "Школа мобильной разработки"
-showColMobDevelp = function (){
-		$('.dev').css("display","none");
-		$('.mob-dev').css("display","table");
-		$('.mob-des').css("display","none");
-		$('.empty').css("display","none");
-}
-//Показать расписание "Школа мобильного дизайна"
-showColMobDesign = function (){
-		$('.dev').css("display","none");
-		$('.mob-dev').css("display","none");
-		$('.mob-des').css("display","table");
-		$('.empty').css("display","none");
-}
-
 $(document).ready(function() {
 
-	// $('.portfolio-slider').slick({
-	// 	centerMode: true,
-	// 	centerPadding: '60px',
-	// 	slidesToShow: 2,
-	// 	responsive: [
-	// 		{
-	// 			breakpoint: 768,
-	// 			settings: {
-	// 				arrows: false,
-	// 				centerMode: true,
-	// 				centerPadding: '40px',
-	// 				slidesToShow: 3
-	//   			}
-	// 		},
-	// 		{
-	// 			breakpoint: 480,
-	// 			settings: {
-	// 				arrows: false,
-	// 				centerMode: true,
-	// 				centerPadding: '40px',
-	// 				slidesToShow: 1
-	//   			}
-	// 		}
- //  		]
-	// });
-
 	$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: true,
+		centerMode: true,
+		focusOnSelect: true
+	});
 
 	//Скролл до выбранного элемента при клике в шапке страницы
 	$('.scrollTo-about').click(function(){
+		$("html, body").animate({ scrollTop: $('#about').offset().top-100}, 1000);
+		$('#popup-menu-toggle').prop("checked", false);
+	});
+	$('.scroll-down-mouse').click(function(){
 		$("html, body").animate({ scrollTop: $('#about').offset().top-100}, 1000);
 		$('#popup-menu-toggle').prop("checked", false);
 	});
@@ -86,6 +39,17 @@ $('.slider-nav').slick({
 	$('.scrollTo-portfolio').click(function(){
 		$("html, body").animate({ scrollTop: $('#portfolio').offset().top-100}, 1000);
 		$('#popup-menu-toggle').prop("checked", false);
+	});
+
+	$('.scrollTo-footer').click(function(){
+		$("html, body").animate({ scrollTop: $('#footer').offset().top-100}, 1000);
+		$('#popup-menu-toggle').prop("checked", false);
+	});
+
+
+	$(".header-menu li").click(function() {
+		$(".header-menu li").removeClass('active');
+		$(this).addClass("active");
 	});
 
 	//При ширине экрана <500px, при клике в списке школ на элемент, показать соответствующюю школу
@@ -107,29 +71,6 @@ $('.slider-nav').slick({
 	//___________media запросы___________
 	$(window).resize(function(){
 		var windowwidth = $(window).width();
-
-		if(windowwidth>500){
-
-			//скрыть меню списка школ при экране в >500px
-			$('#schools-list-toggle').prop("checked", false);
-
-			$('.dev').css("display","");
-			$('.mob-dev').css("display","");
-			$('.mob-des').css("display","");
-			$('.empty').css("display","");
-		}
-	//При выборе расписания школы, при ресайзе сначала >500px потом обратно <500px, показывать расписание выбранной школы
-		else{
-			if($('#develp-intreface').is(':checked')==true){
-				showColDevelpInterface();
-			}
-			if($('#mob-develp').is(':checked')==true){
-				showColMobDevelp();
-			}
-			if($('#mob-design').is(':checked')==true){
-				showColMobDesign();
-			}
-		}
 	});
 });
 
